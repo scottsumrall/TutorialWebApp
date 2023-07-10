889dc76ee6f86434ce8101b4dc2aced28389bb80
scottsumrall <ssumrall@elon.edu>
2023-07-10 16:15:16 -0400

    Added API server base

---
+++ GameStore.Server/Program.cs
@@ -0,0 +1,37 @@
+using TutorialWebApp.Models;
+
+List<Game> games = new()
+{
+    new Game()
+    {
+        Id = 1,
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991, 2, 1)
+    },
+    new Game()
+    {
+        Id = 2,
+        Name = "Final Fantasy XIV",
+        Genre = "RolePlaying",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 9, 30)
+    },
+    new Game()
+    {
+        Id = 3,
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 9, 7)
+    }
+};
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// GET /games
+app.MapGet("/games", () => games);
+
+app.Run();
