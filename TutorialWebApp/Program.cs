0e6870397be3177fc05740eeefaa4329dc582faa
scottsumrall <ssumrall@elon.edu>
2023-07-10 23:35:41 -0400

    Added Game Client to scope

--- TutorialWebApp/Program.cs
+++ TutorialWebApp/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7290") });
+builder.Services.AddScoped<GameClient>();
 
 await builder.Build().RunAsync();
