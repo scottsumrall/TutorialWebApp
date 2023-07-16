ae37f7874ace6122597e6ad62ed40b84a9d26a7f
ssumrall <ssumrall@elon.edu>
2023-07-16 15:59:30 -0400

    Created GameStoreContext with DbSet

---
+++ GameStore.Server/Data/GameStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorialWebApp.Models;
+
+namespace GameStore.Server.NewFolder
+{
+	public class GameStoreContext : DbContext
+	{
+        public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
