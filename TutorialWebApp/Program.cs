dff1b393ed18b7d98d2acd20663109f239cdc0c9
ssumrall <ssumrall@elon.edu>
2023-07-18 13:35:27 -0400

    Updated URL

--- TutorialWebApp/Program.cs
+++ TutorialWebApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7290") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5269") });
 builder.Services.AddScoped<GameClient>();
 
 await builder.Build().RunAsync();
